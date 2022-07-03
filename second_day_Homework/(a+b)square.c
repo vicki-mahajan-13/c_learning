6aa0c5914570cd6f287700fd90c37d493078d784
Vicky Mahajan <mahajanvickyaa78@gmail.com>
2022-07-03 12:10:06 +0530

    a+b's_square

---
+++ second_day_Homework/(a+b)square.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the value of a and b\n");
+    scanf("%d%d",&a,&b);
+    c=pow((a+b),2);
+    printf("Answer is %d",c);
+    return  0;
+}
+
