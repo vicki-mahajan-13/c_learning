809525a2bb00a061fc4c349ab3571eb676c12f5c
Vicky Mahajan <mahajanvickyaa78@gmail.com>
2022-07-04 15:13:26 +0530

    area_of_circle

---
+++ second_day_Homework/area_of_circle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float a,b;
+    printf("Enter radius of circle");
+    scanf("%f",&a);
+    b=3.14*(pow((a),2));
+    printf("Area of circle is %f",b);
+    return 0;
+}
