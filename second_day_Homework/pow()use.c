0093692eb9506776596a644d0e9987686e8e15e9
Vicky Mahajan <mahajanvickyaa78@gmail.com>
2022-07-04 14:19:33 +0530

    pow()_use

---
+++ second_day_Homework/pow()use.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the number");
+    scanf("%d",a);
+    printf("Enter the power");
+    scanf("%d",b),
+    c=pow((a),b);
+    printf("%d",c);
+    return 0;
+}
+
