ffbbc247c8d1fc6fdb8f382195051ed0a1282b38
Vicky Mahajan <mahajanvickyaa78@gmail.com>
2022-07-04 15:14:19 +0530

    sum_five_digits

---
+++ second_day_Homework/sum_of_five_digits.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c,d,e,add;
+    printf("Enter five digits for sum\n");
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    add=a+b+c+d+e;
+    printf("Sum of all digits is %d",add);
+    return 0;    
+}
+
+
