230c3cc984ca31e1862231e1f4baf24ceeadd310
Vicky Mahajan <mahajanvickyaa78@gmail.com>
2022-07-04 14:30:50 +0530

    sqrt()

--- second_day_Homework/sqrt_function.c
+++ second_day_Homework/sqrt_function.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
-    int a,b,c;
+    int a,c;
     printf("Enter the number");
     scanf("%d",&a);
-    printf("Enter the power");
-    scanf("%d",&b),
-    c=pow((a),b);
+    c=sqrt(a);
     printf("%d",c);
     return 0;
 }
+
+
 
