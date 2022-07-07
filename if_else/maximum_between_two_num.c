5aafa26c957c016cfc658894801455723ac4164e
Vicky Mahajan <mahajanvickyaa78@gmail.com>
2022-07-07 14:41:52 +0530

    if_else_marksheet_and_maximum_of_2_num

---
+++ if_else/maximum_between_two_num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter two numbers");
+    scanf("%d%d",&a,&b);
+    if (a>b)
+        printf("%d is maximum",a);
+    else 
+        printf("%d is maximum",b);
+    return 0;
+        
+}
+
+
