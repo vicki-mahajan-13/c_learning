3d9eb36907f9ac17f6aa6acac5b33ce9a17daa16
Vicky Mahajan <mahajanvickyaa78@gmail.com>
2022-07-16 16:55:02 +0530

    updated_calculater_by_using_loop

--- function/calculater.c
+++ function/calculater.c
@@ -20,34 +20,38 @@ int divide (int a, int b)
 
 #include <stdio.h>
 #include <stdlib.h>
-int main()
+void main()
 {
+    
     int a,b,c;
-    printf("1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.Exit\n");
-    printf("Enter your choice\n");
-    scanf("%d",&c);
-    if (c<=5)
+    do
     {
-        if(c>=5)
+        printf("1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.Exit\n");
+        printf("Enter your choice\n");
+        scanf("%d",&c);
+        if (c<=5)
         {
-            exit (0);
-        }
-        printf("Enter two numbers\n");
-        scanf("%d%d",&a,&b);
-        switch(c)
-        {
-            case 1:printf("%d",add(a,b));
-            break;
-            case 2:printf("%d",substract(a,b));
-            break;
-            case 3:printf("%d",multiply(a,b));
-            break;
-            case 4:printf("%d",divide(a,b));
-            break;
-            case 5:exit (0);
-        }
-    } 
-    else
-        printf("Invalid Choice");
-    return 0;
-}
+            if(c>=5)
+            {
+                exit (0);
+            }
+            printf("Enter two numbers\n");
+            scanf("%d%d",&a,&b);
+            switch(c)
+            {
+                case 1:printf("%d\n",add(a,b));
+                break;
+                case 2:printf("%d\n",substract(a,b));
+                break;
+                case 3:printf("%d\n",multiply(a,b));
+                break;
+                case 4:printf("%d\n",divide(a,b));
+                break;
+                case 5:exit (0);
+            }
+        } 
+        else
+            printf("Invalid Choice");    
+    }
+    while(1);
+}      
