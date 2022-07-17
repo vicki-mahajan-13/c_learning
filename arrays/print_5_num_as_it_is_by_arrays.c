9515dcd27969acc65894a06d1e745a9c7817a7fa
Vicky Mahajan <mahajanvickyaa78@gmail.com>
2022-07-17 10:54:47 +0530

    updated

--- arrays/print_5_num_as_it_is_by_arrays.c
+++ arrays/print_5_num_as_it_is_by_arrays.c
@@ -3,9 +3,10 @@ int main()
 {
     int i,a[5];
     printf("Enter five numbers");
-    for(i=0;i<=4;i++)
+    for(i=1;i<=5;i++)
     {
         scanf("%d",&a[i]);
     }
-    printf("%d",i);
+    for(i=1;i<=5;i++)
+        printf("%d",i);
 }
