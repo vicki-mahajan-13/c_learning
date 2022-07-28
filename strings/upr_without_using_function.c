c8a26b7292821a12b6bc317754113b0416566ddb
Vicky Mahajan <mahajanvickyaa78@gmail.com>
2022-07-28 13:26:23 +0530

    updated programs

--- strings/upr_without_using_function.c
+++ strings/upr_without_using_function.c
@@ -7,7 +7,7 @@ int main()
     gets(str);
     for(i=0;str[i]!=0;i++)    
         if(str[i]>='a' && str[i]<='z')
-        str[i]=str[i]-32;
+    str[i]=str[i]-32;
     puts(str);
 return 0;        
    
