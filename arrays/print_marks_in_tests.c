ad641ffc9b5a029e015bd2cdfdb4569c954ee734
Vicky Mahajan <mahajanvickyaa78@gmail.com>
2022-07-29 22:42:32 +0530

    updated_program_of_print_marks_in_tests

--- arrays/print_marks_in_tests.c
+++ arrays/print_marks_in_tests.c
@@ -4,22 +4,29 @@
 int main()
 {
     int a[2000],i,n;
-    printf("how many tests were done\n");
+    char str_arr[1000][20];
+    printf("How many tests were done\n");
     scanf("%d",&n);
-    printf("Enter the marks of that %d tests\n",n);
+    printf("Enter Subject names\n");
+    for(int i=0; i<n; i++)
+     scanf("%s", &str_arr[i]);
+
+    printf("Enter the marks\n");
     for(i=0;i<n;i++)
     {
-        printf("%d : ",i+1);
+        printf("%d.%s : ",i+1,str_arr[i]);
         scanf("%d",&a[i]);
     }
-    printf("marks obtained by a student in %d tests\n",n);
+    printf("Marks obtained by a student in %d tests\n",n);
     for(i=0;i<n;i++)
     {
-        printf("%d\n",a[i]);
+        printf("%d.%s : %d\n",i+1,str_arr[i],a[i]);
     }
 
     return 0;
 }   
+
+   
 
 
     
