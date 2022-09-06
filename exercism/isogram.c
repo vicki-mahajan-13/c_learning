ef7a117d2f7d9a656462d6c11d59ea7bf417924d
Vicky Mahajan <mahajanvickyaa78@gmail.com>
2022-09-06 12:16:36 +0530

    isogram

--- exercism/isogram.c
+++ exercism/isogram.c
@@ -1,14 +1,27 @@
-//Determine if a word or phrase is an isogram
-void is_isogram(char *ptr)
-{
-    printf("%s",ptr);
-}
 
 #include <stdio.h>
-int main()
+#include <stdbool.h>
+#include <string.h>
+bool is_isogram(const char *ptr)
 {
-    char str[100];
-    printf("Enter a string");
-    scanf("%[^/n]s",str);
-    is_isogram(str);
+    long unsigned int i,j,ctr=0;
+    for(i=0;i<strlen(ptr);i++)
+    {
+        ctr=0;
+        for(j=0;j<strlen(ptr);j++)
+        {
+            if(ptr[i]==ptr[j])
+            ctr++;
+        }
+        if (ctr==1)
+        {
+            return true;
+        }
+        if(ctr>1)
+        {
+            return false;
+        }
+
+    }
+return 0;
 }
